20a8be4a52f532a8c85ebf7fa6b11bf142f9c605
samuel <samuel.edmund.morgan@gmail.com>
2024-02-29 12:26:53 +0200

    Added AppData

---
+++ app/src/main/java/com/morgandev/kioskacademy/domain/entity/AppData.kt
@@ -0,0 +1,9 @@
+package com.morgandev.kioskacademy.domain.entity
+
+data class AppData(
+    val backgroundId: Int,
+    val emblemId: Int,
+    val year: Int,
+    val galleryNameUA: String,
+    val galleryNameENG: String
+)
